ce4bead5fd1981babbb9526e84a00aeab66cf30f
varrivoda <varrivoda@yandex.ru>
2024-05-06 14:57:48 +1000

    3. DataExtractors

--- src/main/java/com/example/starter/DataExtractor.java
+++ src/main/java/com/example/starter/DataExtractor.java
@@ -2,7 +2,8 @@ package com.example.starter;
 
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
+import org.springframework.context.ConfigurableApplicationContext;
 
 public interface DataExtractor {
-    Dataset<Row> readData(String pathToData);
+    Dataset<Row> readData(String pathToData, ConfigurableApplicationContext context);
 }
---
+++ src/main/java/com/example/starter/JsonDataExtractor.java
@@ -0,0 +1,13 @@
+package com.example.starter;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.springframework.context.ConfigurableApplicationContext;
+
+public class JsonDataExtractor implements DataExtractor {
+    @Override
+    public Dataset<Row> readData(String pathToData, ConfigurableApplicationContext context) {
+        return context.getBean(SparkSession.class).read().json(pathToData);
+    }
+}
