da47fc9565b24ac90dcc62a0d10cdb4ca027bd31
varrivoda <varrivoda@yandex.ru>
2024-05-19 23:09:36 +1000

    7.1 making WordsMatcher.findAndRemoveMatchingPiecesIfExists()

---
+++ src/main/java/com/example/starter/WordsMatcher.java
@@ -0,0 +1,35 @@
+package com.example.starter;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class WordsMatcher {
+    public static String findAndRemoveMatchingPiecesIfExists(Set<String> options, List<String> pieces) {
+        //тут алгоритм
+        StringBuilder match = new StringBuilder(pieces.remove(0));
+        List<String> remainingOptions = options.stream()
+                .filter(option->option.toLowerCase().startsWith(match.toString().toLowerCase()))
+                .collect(Collectors.toList());
+        if(remainingOptions.isEmpty()) return "";
+
+        //фильтр оставил нам "name" и "nameOfGrandma"
+        while (remainingOptions.size()>1){
+            match.append(pieces.remove(0));
+            remainingOptions.removeIf(
+                    option->!option.toLowerCase().startsWith(
+                            match.toString().toLowerCase()
+                    )
+            );
+        }
+
+        //теперь допустим, что у нас есть только nameOfGrandma
+        //но на данном этапе только name - надо прождолжать матчить,пока equals полностью не совпадет
+        while(remainingOptions.get(0).equalsIgnoreCase(match.toString())){
+            match.append(pieces.remove(0));
+        }
+
+        return match.toString();
+    }
+}
